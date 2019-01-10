26c67046dade0c5cb287a8c040587ee020a7c2e6
AlexShalv <46564002+AlexShalv@users.noreply.github.com>
2019-01-10 16:49:53 +0400

    Add files via upload

---
+++ Spirit8/js/mixitup.js
@@ -0,0 +1,17 @@
+$(function(){
+	$("#works").mixItUp({
+		controls: {
+			activeClass: "active"
+		}
+	});
+});
+
+
+$('.slider_testimonials').slick({
+  dots:true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows : false,
+});
---
+++ Spirit8/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+$('.slider').slick({
+  dots:true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows : false,
+});
+
+$('.slider_clients').slick({
+  dots:true,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows : false,
+});
+
+
+
+
+});
+
